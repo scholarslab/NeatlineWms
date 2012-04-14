6973a5af48eb3dee6bd0844f35b74a9650ec19a9
David McClure <david.mcclure@virginia.edu>
2012-04-14 10:29:01 -0400

    browse view

--- views/admin/servers/browse.php
+++ views/admin/servers/browse.php
@@ -13,4 +13,63 @@
  */
 ?>
 
+<?php
+head(array('content_class' => 'neatline'));
+?>
 
+<?php echo $this->partial('index/_header.php', array(
+    'subtitle' => 'Browse Servers',
+    'add_button_uri' => 'neatline-maps/add',
+    'add_button_text' => 'Create a Server'
+)); ?>
+
+<div id="primary">
+
+<?php echo flash(); ?>
+
+<?php if(count($servers) > 0): ?>
+
+<table>
+
+    <thead>
+        <tr>
+        <!-- Column headings. -->
+        <?php browse_headings(array(
+            'Server' => null,
+            'URL' => null,
+            'Namespace' => null,
+            'Status' => null
+        )); ?>
+        </tr>
+    </thead>
+
+    <tbody>
+        <!-- Servers listings. -->
+        <?php foreach ($servers as $server): ?>
+        <tr serverid="<?php echo $server->id; ?>">
+            <td class="title"><?php echo $server->name; ?>
+                <?php echo $this->partial('index/_action_buttons.php', array(
+                  'uriSlug' => 'neatline-maps',
+                  'server' => $server)); ?>
+            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+
+</table>
+
+<?php else: ?>
+
+    <p class="neatline-alert">There are no servers yet.
+    <a href="<?php echo uri('neatline-maps/add'); ?>">Create one!</a></p>
+
+<?php endif; ?>
+
+</div>
+
+<?php
+foot();
+?>
