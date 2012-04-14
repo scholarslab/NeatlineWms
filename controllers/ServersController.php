f2d2d432475dce29b06bd277adcbfcc615f85519
David McClure <david.mcclure@virginia.edu>
2012-04-14 09:25:17 -0400

    shell for servers controller

---
+++ controllers/ServersController.php
@@ -0,0 +1,68 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * Servers controller.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @author      Scholars' Lab <>
+ * @author      David McClure <david.mcclure@virginia.edu>
+ * @copyright   2012 The Board and Visitors of the University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
+ */
+
+class NeatlineWms_ServersController extends Omeka_Controller_Action
+{
+
+    /**
+     * Show servers.
+     *
+     * @return void
+     */
+    public function browseAction()
+    {
+
+    }
+
+    /**
+     * Add server.
+     *
+     * @return void
+     */
+    public function addAction()
+    {
+
+    }
+
+    /**
+     * Edit server.
+     *
+     * @return void
+     */
+    public function editAction()
+    {
+
+    }
+
+    /**
+     * Delete server.
+     *
+     * @return void
+     */
+    public function deleteAction()
+    {
+
+    }
+
+    /**
+     * Set server active.
+     *
+     * @return void
+     */
+    public function activeAction()
+    {
+
+    }
+
+}
